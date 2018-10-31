5ddd6fef891bfd077a2157f299114f5207d14a53
leanfj <leanfj@hotmail.com>
2018-10-30 22:31:05 -0300

    implementado filtro com isotopejs

--- js/index.js
+++ js/index.js
@@ -57,3 +57,50 @@ $menuMobileIcon.addEventListener('click', () => {
     }
   };
 })();
+
+var iso = new Isotope('.thumbs', {
+  itemSelector: '.thumbs__item'
+});
+
+var filtersElem = document.querySelectorAll('.portifolio-navigation__item');
+
+filtersElem.forEach(element => {
+  element.addEventListener('click', function(event) {
+    var filterValue = event.target.getAttribute('data-filter');
+    iso.arrange({ filter: filterValue });
+  });
+});
+
+// change is-checked class on buttons
+let tagGroups = document.querySelectorAll('.portifolio-navigation__item');
+
+//Loop sobre item para atuar em cada elementos
+tagGroups.forEach(item => {
+  //Em cada item adciono um evento de click
+  item.addEventListener('click', function() {
+    //Pego o elemento com a classe que ativa o estilo
+    let current = document.querySelector(
+      '.portifolio-navigation__item--active'
+    );
+    console.log(current.classList);
+    //Troco o
+    current.className = current.className.replace(
+      ' portifolio-navigation__item--active',
+      ''
+    );
+    this.className += ' portifolio-navigation__item--active';
+  });
+});
+
+// for (var i = 0; i < tagGroups.length; i++) {
+//   tagGroups[i].addEventListener('click', function() {
+//     var current = document.getElementsByClassName(
+//       'portifolio-navigation__item--active'
+//     );
+//     current[0].className = current[0].className.replace(
+//       ' portifolio-navigation__item--active',
+//       ''
+//     );
+//     this.className += ' portifolio-navigation__item--active';
+//   });
+// }
